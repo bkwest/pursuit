7b3b5d99dc80709f1a21c728c14ffb5ef0fdf05f
Dylan Markowitz <deeveret@ucsc.edu>
2015-01-10 17:29:44 -0800

    Added swipe motion and mouse drag if not using swipe. Added the shield arc that spawns when you swipe and lets you control its settings.

---
+++ Assets/Scripts/arcShieldScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class arcShieldScript : MonoBehaviour {
+
+	private Vector3 startPosition;
+	public float lifeSpanDist = 10f;
+
+	void Start () {
+		startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 distanceTraveled = transform.position - startPosition;
+		float distTraveled = distanceTraveled.magnitude;
+		if(distTraveled >= lifeSpanDist)
+		{
+			DestroyObject(gameObject);
+			Destroy(this);
+		}
+	}
+}
